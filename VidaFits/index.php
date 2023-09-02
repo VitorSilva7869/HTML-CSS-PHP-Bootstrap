39bc9f43bf2103869385761da95fea58f107e041
Vitor Silva <99691815+VitorSilva7869@users.noreply.github.com>
2023-09-02 14:04:57 -0300

    Add files via upload

---
+++ VidaFits/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>VidaFits</title>
+    <!--Estilo-->
+    <link rel="stylesheet" href="css/indexEstilo.css">
+
+    <!--Fonte google-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Nunito:wght@500&display=swap" rel="stylesheet">
+
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>    
+
+    <!--Kit icones-->
+    <script src="https://kit.fontawesome.com/8e383bc8aa.js" crossorigin="anonymous"></script>
+    
+
+</head>
+<body>
+    <header>
+        <nav class=" ">
+            <div id="sumir" class="d-flex justify-content-between mx-5 mt-2 nav nav-borda">
+
+                <a class="nav-link" href="#">
+                    <img class="sumir" src="imagens/VIDAFIT-removebg-preview.png" alt="" width="120" height="" class="d-inline-block align-text-top">
+                </a>
+                <ul class="sumir">
+                    <li class="nav-link"><a  href="#">Home</a></li>
+                    <li class="nav-link"><a  href="#">Meditar</a></li>
+                    <li class="nav-link"><a  href="#">Musculação</a></li>
+                </ul>
+
+                <div class="icone sumir">
+                    <a href="">
+                        <i class="fa-solid fa-magnifying-glass fa-xl "></i>
+                    </a>
+                    <a href="">
+                        <i class="fa-regular fa-user fa-xl"></i>
+                    </a>
+                </div>
+                
+            </div>
+            <div class="d-flex sumir justify-content-between mx-5 mt-2 nav nav-borda">
+                <button class="" id="hamburgue"><i class="fa-solid fa-bars fa-2xl"></i></button>
+                <button class="" id="hamburgue"><i class="fa-solid fa-bars fa-2xl"></i></button>
+
+            </div>
+        </nav>  
+    </header>
+    
+    <section>
+        <h1>O estilo do Bagulho</h1>
+        <h2>O estilo do Bagulho</h2>
+        <div class="oi"><a href="">oiiiiiiiiiiiiiiiiiii</a></div>
+    </section>
+</body>
+</html>
